bc45c56aab6e8b55c0c564f310295a25576958e9
Caroline <lipowska.2003@gmail.com>
2025-05-14 13:54:12 +0200

    Basic UI

--- src/main/java/com/cori/typemaster/GameApplication.java
+++ src/main/java/com/cori/typemaster/GameApplication.java
@@ -4,17 +4,46 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.input.KeyCode;
 
 import java.io.IOException;
 
 public class GameApplication extends Application {
+
+    private static final int SCENE_WIDTH = 1280;
+    private static final int SCENE_HEIGHT = 720;
+    private static final String APP_TITLE = "TypeMaster";
+    private static final String MAIN_SCREEN_FXML = "main-screen.fxml";
+
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(GameApplication.class.getResource("game-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1280, 720);
-        stage.setTitle("TypeMaster");
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage primaryStage) throws IOException {
+        primaryStage.setTitle(APP_TITLE);
+
+        FXMLLoader fxmlLoader = new FXMLLoader(GameApplication.class.getResource(MAIN_SCREEN_FXML));
+        Scene scene = new Scene(fxmlLoader.load(), SCENE_WIDTH, SCENE_HEIGHT);
+
+        primaryStage.setScene(scene);
+        primaryStage.setFullScreen(true); // Ustawienie domyślnego trybu pełnoekranowego
+
+        // Obsługa F11 do włączania/wyłączania pełnoekranowego
+        scene.setOnKeyPressed(event -> {
+            if (event.getCode() == KeyCode.F11) {
+                toggleFullScreen(primaryStage);
+            }
+        });
+
+        primaryStage.show();
+    }
+
+    // Metoda pomocnicza do przełączania trybu pełnoekranowego
+    private void toggleFullScreen(Stage stage) {
+        if (stage.isFullScreen()) {
+            stage.setFullScreen(false);
+            stage.setWidth(SCENE_WIDTH);
+            stage.setHeight(SCENE_HEIGHT);
+        } else {
+            stage.setFullScreen(true);
+        }
     }
 
     public static void main(String[] args) {
---
+++ src/main/java/com/cori/typemaster/ModeSelectController.java
@@ -0,0 +1,4 @@
+package com.cori.typemaster;
+
+public class ModeSelectController {
+}
